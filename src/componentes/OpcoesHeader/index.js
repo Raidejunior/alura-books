4d8d393e54021aada8500dacabb66709f6ababd1
Raide Junior <raidejunior@gmail.com>
2024-06-05 08:45:16 -0300

    feat: Inserido opcoes do header

---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,13 @@
+function OpcoesHeader() {
+    return (
+        <ul className='opcoes'>
+          {
+            textoOpcoes.map( (texto) => (
+              <li className='opcao'><p>{texto}</p></li>
+            )
+          )}
+        </ul>
+    )
+}
+
+export default OpcoesHeader
